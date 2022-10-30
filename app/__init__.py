a7d4ab784d5896b1746d60d947d233ec449b18b9
Ethan Heckard <eheckard23@gmail.com>
2022-10-30 11:42:57 -0700

    Flask-Migrate render as batch to support SQL Alter

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,18 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from app.database import db, migrate
 from app.playlistfinder import playlist_finder_blueprint
 from dotenv import load_dotenv
 
 load_dotenv()
 
-db = SQLAlchemy()
-migrate = Migrate()
-
 def create_app():
 	app = Flask(__name__)
 	app.register_blueprint(playlist_finder_blueprint)
 	app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-	from app.models import Artist, Owner, Playlist, Song, Usage_Statistic, User
+	from app.models import Artist, Owner, Playlist, Song, Song_In_Playlist, Usage_Statistic, User
 
 	db.init_app(app)
-	migrate.init_app(app, db)
+	migrate.init_app(app, db, render_as_batch=True)
 
 	return app
